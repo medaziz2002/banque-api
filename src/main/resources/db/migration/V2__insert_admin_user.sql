8b07cc305e10421dc04a08ed0e023f7296e5f198
medaziz2002 <azizhassine673@gmail.com>
2025-08-10 15:19:01 +0200

    change file insert-admin

--- src/main/resources/db/migration/V2__insert_admin_user.sql
+++ src/main/resources/db/migration/V2__insert_admin_user.sql
@@ -1,14 +1,17 @@
-insert into role (id, name)
-values (nextval('role_seq'), 'ROLE_USER');
 
-insert into role (id, name)
-values (nextval('role_seq'), 'ROLE_ADMIN');
+-- Insert roles
+insert into role (id, name) values (nextval('role_seq'), 'ROLE_USER');
+insert into role (id, name) values (nextval('role_seq'), 'ROLE_ADMIN');
 
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
+-- Insert admin user with pre-hashed password
+-- Note: Using pre-hashed password instead of pgcrypto for H2 compatibility
+insert into "user" (id, active, email, firstname, lastname, password)
+values (nextval('user_seq'), true, 'admin@aziz.com', 'Aziz', 'Belhaj Hssine',
+        '$2a$10$3ek/PrpenD0.ppuO7Jp7HulcpTqpcUrETeKbIVwUrU139n5Co3nZO');
 
-insert into _user (id, active, email, firstname, lastname, password)
-values (nextval('_user_seq'), true, 'admin@aziz.com', 'Aziz', 'Belhaj Hssine', '$2a$10$3ek/PrpenD0.ppuO7Jp7HulcpTqpcUrETeKbIVwUrU139n5Co3nZO');
-
-
-insert into _user_roles (users_id, roles_id)
-values ((select id from _user where email = 'admin@aziz.com'), (select id from role where name = 'ROLE_ADMIN'));
+-- Assign admin role to user
+insert into user_roles (users_id, roles_id)
+values (
+           (select id from "user" where email = 'admin@aziz.com'),
+           (select id from role where name = 'ROLE_ADMIN')
+       );
